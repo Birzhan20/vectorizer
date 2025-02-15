d2fbad4f030dece1a1fc9d7a6a5bc17d15bc5607
birzhan <birzhan981@gmail.com>
2025-02-15 19:30:00 +0500

    fixed

--- app/vectorizer.py
+++ app/vectorizer.py
@@ -1,6 +1,6 @@
 from sentence_transformers import SentenceTransformer
 
-local_model_path = "./model"
+local_model_path = "../model"
 model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 model.save(local_model_path)
 vectorizer = SentenceTransformer(local_model_path)
